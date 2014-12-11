5dc041084a30cf2f3ed4a533c54c243619553f81
iamquickster <feo.pascal@gmail.com>
2014-12-10 21:18:57 -0500

    Item Set

---
+++ INF4230_tp3/src/aiplaylist/ItemSet.java
@@ -0,0 +1,69 @@
+package aiplaylist;
+
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ItemSet extends AbstractSet<Item> implements Comparable<ItemSet> {
+
+	List<Item> items;
+	String id = "";
+
+	public ItemSet(List<Item> items) {
+		this.items = new LinkedList<Item>(items);
+		Collections.sort(this.items);
+		for (Item i : this.items) {
+			id += i.getId();
+		}
+
+	}
+
+	public ItemSet(Item i) {
+		items = new LinkedList<Item>();
+		items.add(i);
+		id += i.getId();
+	}
+
+	public ItemSet(ItemSet i) {
+		this(new ArrayList<Item>(i));
+	}
+
+	public ItemSet(Collection<Item> items) {
+		this(new LinkedList<Item>(items));
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		return items.iterator();
+	}
+
+	@Override
+	public int size() {
+		return items.size();
+	}
+
+	@Override
+	public int compareTo(ItemSet o) {
+		return this.id.compareTo(o.getId());
+	}
+
+	private String getId() {
+		return id;
+	}
+
+	public ItemSet join(ItemSet other) {
+		List<Item> tmp = new LinkedList<Item>(items);
+		for (Item j : other) {
+			if (!tmp.contains(j)) {
+				tmp.add(j);
+			}
+		}
+
+		return new ItemSet(tmp);
+	}
+
+}
