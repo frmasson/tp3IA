42172a57975491a3394eab407928693400f15963
marex <noware22@gmail.com>
2014-12-12 20:58:00 -0500

    Modification de profile et history avec Item
    Le profile et l'historique functionne avec des Items au lieu de Song.

--- INF4230_tp3/src/aiplaylist/History.java
+++ INF4230_tp3/src/aiplaylist/History.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class History {
-	private List<Song> playedSongs = new ArrayList<Song>();
-	private List<Song> skippedSongs = new ArrayList<Song>();
-	private List<Song> history = new ArrayList<Song>();
+	private List<Item> playedSongs = new ArrayList<Item>();
+	private List<Item> skippedSongs = new ArrayList<Item>();
+	private List<Item> history = new ArrayList<Item>();
 	
-	public void addSong(Song s, boolean played) {
+	public void addSong(Item s, boolean played) {
 		if (played) {
 			playedSongs.add(s);
 		} else {
@@ -18,43 +18,43 @@ public class History {
 		history.add(s);
 	}
 	
-	public Song getLastPlayedSong() {
-		return getLastPlayedSong(1);
+	public Item getLastPlayedItem() {
+		return getLastPlayedItem(1);
 	}
 	
-	public Song getLastPlayedSong(int n) {
+	public Item getLastPlayedItem(int n) {
 		return playedSongs.get(playedSongs.size() - n);
 	}
 	
-	public Song getLastSkippedSong() {
-		return getLastSkippedSong(1);
+	public Item getLastSkippedItem() {
+		return getLastSkippedItem(1);
 	}
 	
-	public Song getLastSkippedSong(int n) {
+	public Item getLastSkippedItem(int n) {
 		return skippedSongs.get(playedSongs.size() - n);
 	}
 	
 	/**
 	 * @return Toutes les chançons joué depuis l'ouverture du programme
 	 */
-	public List<Song> getPlayedSongs() {
-		return new ArrayList<Song>(playedSongs);
+	public List<Item> getPlayedSongs() {
+		return new ArrayList<Item>(playedSongs);
 		
 	}
 	
 	/**
 	 * @return Toutes les chançons sauté depuis l'ouverture du programme
 	 */
-	public List<Song> getSkippedSongs() {
-		return new ArrayList<Song>(skippedSongs);
+	public List<Item> getSkippedSongs() {
+		return new ArrayList<Item>(skippedSongs);
 		
 	}
 	
 	/**
 	 * @return La sequence complete de chançons depuis l'ouverture du programme
 	 */
-	public List<Song> getSongsSequence() {
-		return new ArrayList<Song>(history);
+	public List<Item> getSongsSequence() {
+		return new ArrayList<Item>(history);
 		
 	}
 }
--- INF4230_tp3/src/aiplaylist/Profile.java
+++ INF4230_tp3/src/aiplaylist/Profile.java
@@ -3,19 +3,23 @@ package aiplaylist;
 import java.util.List;
 
 public class Profile {
+	
 	private History history = new History();
-	public List<Song> getFrame(int frame) {
+	
+	public List<Item> getFrame(int frame) {
 		int max = history.getPlayedSongs().size();
 		if (frame > max) frame = max;
 		
 		return history.getPlayedSongs().subList(max - frame, max);
 		
 	}
+	
 	public History getHistory() {
 		return history;
 		
 	}
-	public void add(Song s, boolean played) {
+	
+	public void add(Item s, boolean played) {
 		history.addSong(s, played);
 	}
 }
