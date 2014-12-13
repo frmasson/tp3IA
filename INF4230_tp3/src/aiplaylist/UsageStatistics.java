ea2a36e96099f04a52df687a7c778c0b9e6a680c
marex <noware22@gmail.com>
2014-12-12 20:56:08 -0500

    Output des stats dans un fichiers

--- INF4230_tp3/src/aiplaylist/UsageStatistics.java
+++ INF4230_tp3/src/aiplaylist/UsageStatistics.java
@@ -1,24 +1,33 @@
 package aiplaylist;
 
-import java.util.LinkedList;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsageStatistics {
 
 	private class Row {
 		protected long time;
-		protected boolean skipped;
+		protected int skipped;
 		
-		public Row(long elapseTime, boolean skipped) {
+		public Row(long elapseTime, int skipped) {
 			this.time = elapseTime;
 			this.skipped = skipped;
 		}
 		
 	}
 	
-	private List<Row> rows = new LinkedList<Row>();
+	private List<Row> rows = new ArrayList<Row>();
 	private long startingTime;
 	private long endingTime;
+	
+	public UsageStatistics() {
+		rows.add(new Row(0, 0));
+	}
 	
 	public void setStartingTime() {
 		startingTime = System.currentTimeMillis();
@@ -29,7 +38,12 @@ public class UsageStatistics {
 	}
 	
 	public void notify(boolean played) {
-		rows.add(new Row(getElapseTime(System.currentTimeMillis()), !played));
+		long elapseTime = getElapseTime(System.currentTimeMillis());
+		if (rows.get(rows.size() - 1).time == elapseTime) {
+			if (!played) rows.get(rows.size() - 1).skipped += 1;
+		} else {
+			rows.add(new Row(elapseTime, !played ? 1 : 0));
+		}
 	}
 	
 	private long getElapseTime(long time) {
@@ -37,18 +51,28 @@ public class UsageStatistics {
 	}
 	
 	public void exportStatistics() {
-		//TODO output to a file
-		String line;
+		Writer writer;
+		String lines = "";
 		String header = "Time,Skipped,TotalTime\n";
 		
 		System.out.print(header);
 		
 		for(Row row : rows) {
-			int n = 0;
-			if (row.skipped) n = 1;
-			line = row.time + "," + n;
-			line += "," + getElapseTime(endingTime) + "\n";
-			System.out.print(line);
+			lines += row.time + "," + row.skipped;
+			lines += "," + getElapseTime(endingTime) + "\n";
 		}
+		
+		System.out.print(lines);
+		
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Stats.csv")));
+			writer.write(header);
+			writer.write(lines);
+			writer.close();
+		} catch (IOException ex) {
+			System.err.println("Error while writing stats to file !");
+		}
+		
+		
 	}
 }
